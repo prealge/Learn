d38f7a3b5be140b963321b15848bc87dcf054c0c
prealge <34986064+prealge@users.noreply.github.com>
2023-01-10 21:46:14 -0500

    Add Vocab List #6

---
+++ js/multchoice.js
@@ -0,0 +1,2 @@
+// Defines HTML elements
+var btn1 = document.getElementById("btn1"), btn2 = document.getElementById("btn2"), btn3 = document.getElementById("btn3"), btn4 = document.getElementById("btn4");
